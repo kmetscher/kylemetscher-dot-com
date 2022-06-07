592d4c0de6a37b6ef0f3624bbb264c9b4af1b9a1
Kyle Metscher <kscott.mets@gmail.com>
2022-06-06 23:42:24 -0700

    Sidebar tags generated dynamically

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,22 +1,35 @@
-<?php echo
-'  <div class="sidebar">
-    <h3>Tags</h3>
-    <div class="tags">
-      <ol class="tags">
-      </ol>
-    </div>
-    <h3>Archives</h3>
-    <div class="archives">
-      <ol class="archives">
-      </ol>
-    </div>
-    <h3>Language</h3>
-    <div class="languages">
-      <ul class="languages">
-        <li><a href="#">English</a></li>
-        <li><a href="#">Magyar</a></li>
-        <li><a href="#">Deutsch</a></li>
-      </ul>
-    </div>
-  </div>'
-  ;?>
+<?php require_once('includes/connect.php') ?>
+
+<?php  // fetch all tags in the tags table in alphabetical order
+$sideTagsQuery = "SELECT * FROM tags ORDER BY name ASC";
+$sideTagsAnswer = $conn->query($sideTagsQuery);
+?>
+
+<?php
+echo '<div class="sidebar">';
+    echo '<h3>Tags</h3>';
+    echo '<div class="tags">';
+      echo '<ol class="tags">';
+
+      // echo an anchor for each row returned by associative fetch
+      while($sideTagsRow = mysqli_fetch_assoc($sideTagsAnswer)) {
+        echo '<a href="viewtag.php?'.$sideTagsRow['id'].'"><li>'.$sideTagsRow['name'].'</li></a>';
+      } // nigh identical to post preview filedunder 
+
+      echo '</ol>';
+    echo '</div>';
+    echo '<h3>Archives</h3>';
+    echo '<div class="archives">';
+      echo '<ol class="archives">';
+      echo '</ol>';
+    echo '</div>';
+    echo '<h3>Language</h3>';
+    echo '<div class="languages">';
+      echo '<ul class="languages">';
+        echo '<li><a href="#">English</a></li>';
+        echo '<li><a href="#">Magyar</a></li>';
+        echo '<li><a href="#">Deutsch</a></li>';
+      echo '</ul>';
+    echo '</div>';
+  echo '</div>';
+?>
