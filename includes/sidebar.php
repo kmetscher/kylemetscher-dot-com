4acc6fa494c9fdff36c9f4d8a5e26d32f60450a4
Kyle Metscher <kscott.mets@gmail.com>
2022-06-15 21:17:49 -0700

    sidebar PHP cleanup

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,40 +1,34 @@
-<?php require_once('includes/connect.php') ?>
-
-<?php  // fetch all tags in the tags table in alphabetical order
+<?php require_once('includes/connect.php');
+// fetch all tags in the tags table in alphabetical order
 $sideTagsQuery = "SELECT * FROM tags ORDER BY name ASC";
 $sideTagsAnswer = $conn->query($sideTagsQuery);
-?>
-
-<?php  // fetch all pubdates in the blog_posts table by recency
+// fetch all pubdates in the blog_posts table by recency
 $archiveQuery = "SELECT date FROM blog_posts ORDER BY date DESC";
 $archiveAnswer = $conn->query($archiveQuery);
 ?>
-
-<?php
-echo '<div class="sidebar">';
-    echo '<a href="viewalltags.php"><h3>Tags</h3></a>';
-    echo '<div class="tags">';
-      echo '<ol class="tags">';
-
-      // echo an anchor for each row returned by associative fetch
-      while($sideTagsRow = mysqli_fetch_assoc($sideTagsAnswer)) {
-        echo '<a href="viewtag.php?'.$sideTagsRow['id'].'"><li>'.$sideTagsRow['name'].'</li></a>';
-      } // nigh identical to post preview filedunder
-
-      echo '</ol>';
-    echo '</div>';
-    echo '<h3><a href="#">Archives</a></h3>';
-    echo '<div class="archives">';
-      echo '<ol class="archives">';
-      echo '</ol>';
-    echo '</div>';
-    echo '<h3>Language</h3>';
-    echo '<div class="languages">';
-      echo '<ul class="languages">';
-        echo '<li><a href="viewlang.php?en">English</a></li>';
-        echo '<li><a href="viewlang.php?hu">Magyar</a></li>';
-        echo '<li><a href="viewlang.php?de">Deutsch</a></li>';
-      echo '</ul>';
-    echo '</div>';
-  echo '</div>';
-?>
+<div class="sidebar">
+  <a href="viewalltags.php"><h3>Tags</h3></a>
+  <div class="tags">
+    <ol class="tags">
+    <?php
+    // echo an anchor for each row returned by associative fetch
+    while($sideTagsRow = mysqli_fetch_assoc($sideTagsAnswer)) {
+      echo '<a href="viewtag.php?'.$sideTagsRow['id'].'"><li>'.$sideTagsRow['name'].'</li></a>';
+    } // nigh identical to post preview filedunder
+      ?>
+    </ol>
+  </div>
+  <h3><a href="#">Archives</a></h3>
+  <div class="archives">
+    <ol class="archives">
+    </ol>
+  </div>
+  <h3>Language</h3>
+  <div class="languages">
+    <ul class="languages">
+      <li><a href="viewlang.php?en">English</a></li>
+      <li><a href="viewlang.php?hu">Magyar</a></li>
+      <li><a href="viewlang.php?de">Deutsch</a></li>
+    </ul>
+  </div>
+</div>
