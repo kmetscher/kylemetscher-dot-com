33359c0f97629788cfecb136b0e42bb4e1612564
Kyle Metscher <kscott.mets@gmail.com>
2022-06-17 16:19:41 -0700

    Edit post backend functional

--- gutenberg/gutenberg.php
+++ gutenberg/gutenberg.php
@@ -3,9 +3,9 @@
   <title>Gutenberg | Kyle Metscher</title>
 </head>
 <body>
-  <?php ?>
+  <?php $heyGitHub = "can you stop labeling this as hacklang please"; ?>
   <h1><a href="writepost.php">Write</a></h1>
-  <h1><a href="editpost.php">Edit</a></h1>
+  <h1><a href="selectpost.php">Edit</a></h1>
   <h1><a href="#">Delete</a></h1>
   <h1><a href="imageupload.php">Image upload</a></h1>
 </body>
